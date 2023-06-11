0f5cc24d416e999cdfd240e45199f59a434a86d0
Vitalii Popov <vetlo@yandex.ru>
2023-06-11 12:51:41 +0300

    Функция для нахождения мах из 9

---
+++ Lesson05/Max9/Program.cs
@@ -0,0 +1,27 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2> result) result = arg2;
+    if(arg3> result) result = arg3;
+    return result;
+}
+int a1 = 15;
+int b1 = 24;
+int c1 = 27;
+int a2 = 161;
+int b2 = 14;
+int c2 = 171;
+int a3 = 19;
+int b3 = 34;
+int c3 = 37;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
