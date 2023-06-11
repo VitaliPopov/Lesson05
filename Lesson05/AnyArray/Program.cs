4d328ff06f80d528146cbc5e0064ee9ed377894f
Vitalii Popov <vetlo@yandex.ru>
2023-06-11 13:21:56 +0300

    Нахождение числа в массиве

---
+++ Lesson05/AnyArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 11, 24, 31, 41, 51, 24, 34, 27, 18 };
+
+int n = array.Length;
+int find = 24;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+    }
